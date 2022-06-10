cd73a710caf449d3236a1245ac47a41b38359700
Oginn <oginniadeniyi@gmail.com>
2022-06-09 21:46:19 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,18 @@
 #include "main.h"
 
 /**
- * print_most_numbers -  checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
  */
 void print_most_numbers(void)
 {
-	int a;
+	int ch;
 
-	for (a = 48; a < 58; a++)
+	for (ch = 48; ch < 58; ch++)
 	{
-		if (a != 50)
-		{
-			if (a != 52)
-			{
-			_putchar(a);
-			}
-		}
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
 	}
 	_putchar('\n');
 }
