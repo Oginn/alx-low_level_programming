8e3f7f8dfcd426130f47d926f6865afdd4bc2405
Oginn <oginniadeniyi@gmail.com>
2022-06-30 12:15:59 -0700

    that multiplies two positive number

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,98 +1,158 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * is_digit - checks for non-digit char
- * @s: string to be evaluate
- * Return: 0 if a non-digit is found, 1 otherwise
+ * main - program that multiplies two positive numbers
+ *
+ * @argc: argument count, must be 3
+ * @argv: arguments, argv[1] and argv[2]
+ *
+ * Return: product of argv[1] by argv[2]
  */
-int is_digit(char *s)
-{
-	int a = 0;
 
-	while (s[a])
+int main(int argc, char *argv[])
+{
+	char *num1, *num2;
+	int i, j, k, len1, len2, len, d1, d2, d1d2, carry, *mul;
+
+	if (argc != 3 || !(_isnumber(argv[1])) || !(_isnumber(argv[2])))
+		_error(), exit(98);
+	num1 = argv[1], num2 = argv[2];
+	len1 = _strlen(num1), len2 = _strlen(num2), len = len1 + len2;
+	mul = _calloc(len, sizeof(int));
+	if (mul == NULL)
+		exit(98);
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		if (s[a] < '0' || s[a] > '9')
+		d1 = num1[i] - '0';
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			d2 = num2[j] - '0';
+			d1d2 = d1 * d2;
+			mul[i + j + 1] += d1d2 % 10;
+			carry = d1d2 / 10;
+			if (mul[i + j + 1] > 9)
+			{
+				mul[i + j] += mul[i + j + 1] / 10;
+				mul[i + j + 1] = mul[i + j + 1] % 10;
+			}
+			mul[i + j] += carry;
+		}
+	}
+	for (k = 0; mul[k] == 0 && k < len; k++)
+		;
+	if (k == len)
+		_putchar(mul[len - 1] + '0');
+	else
+	{
+		for (i = k; i < len; i++)
+			_putchar(mul[i] + '0');
+	}
+	_putchar('\n');
+	free(mul);
+	return (0);
+}
+
+/**
+ * _isnumber - checks for digit-only (0 through 9) numbers
+ *
+ * @str: parameter hard-coded in main
+ *
+ * Return: 1 or 0
+ */
+
+int _isnumber(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
 			return (0);
-		a++;
 	}
 	return (1);
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- * Return: the length of the string
+ * _error - print error
+ * Return: void
  */
+
+void _error(void)
+{
+	int i;
+	char error[] = "Error";
+
+	for (i = 0; i < 5; i++)
+		_putchar(error[i]);
+	_putchar('\n');
+}
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: parameter defined in main
+ *
+ * Return: length of string
+ */
+
 int _strlen(char *s)
 {
-	int a = 0;
+	int i = 0;
 
-	while (s[a] != '\0')
+	while (*s != '\0')
 	{
-		a++;
+		i++;
+		s++;
 	}
-	return (a);
+	return (i);
 }
 
 /**
- * errors - handles errors for main
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: size of the memory space to allocate in bytes
+ * @size: size of type
+ * Return: void pointer
  */
-void errors(void)
+
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	printf("Error\n");
-	exit(98);
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	_memset(ptr, 0, size * nmemb);
+	return (ptr);
 }
 
 /**
- * main - multiplies to numbers
- * @argv: array of args
- * @argc: numer of args
- * Return: 0 success
+ * _memset - function that fills memory with a constant byte
+ *
+ * @s: parameter defined in main, pointer to memory area
+ * @b: parameter defined in main, constant byte
+ * @n: parameter defined in main, number of bytes to be filled
+ *
+ * Return: memory address of function (memory area)
  */
-int main(int argc, char *argv[])
+
+char *_memset(char *s, char b, unsigned int n)
 {
-	char *s1, *s2;
-	int a, b, leng, leng1, leng2, c, dig1, dig2, *rs;
+	unsigned int i;
+	char *tmp = s;
 
-	a = 0;
-
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	leng1 = _strlen(s1);
-	leng2 = _strlen(s2);
-	leng = leng1 + leng2 + 1;
-	rs = malloc(sizeof(int) * leng);
-	if (!rs)
-		return (1);
-	for (b = 0; b <= leng1 + leng2; b++)
-		rs[b] = 0;
-	for (leng1 = leng1 - 1; leng1 >= 0; leng1--)
+	for (i = 0; i < n; i++)
 	{
-		dig1 = s1[leng1] - '0';
-		c = 0;
-		for (leng2 = _strlen(s2) - 1; leng2 >= 0; leng2--)
-		{
-			dig2 = s2[leng2] - '0';
-			c += rs[leng1 + leng2 + 1] + (dig1 * dig2);
-			rs[leng1 + leng2 + 1] = c % 10;
-			c /= 10;
-		}
-		if (c > 0)
-			rs[leng1 + leng2 + 1] += c;
+		*s = b;
+		s++;
 	}
-	for (b = 0; b < leng - 1; b++)
-	{
-		if (rs[b])
-			a = 1;
-		if (a)
-			_putchar(rs[b] + '0');
-	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(rs);
-	return (0);
+	s = tmp;
+	return (s);
 }
